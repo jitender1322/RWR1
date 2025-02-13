f82ed669d68414de927fd8068bfebfafdf1eed4d
jitender1322 <jitendersarswat6@gmail.com>
2025-02-13 17:39:26 +0530

    practice

--- React5to6/reactbatch/src/App.jsx
+++ React5to6/reactbatch/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import LocalCrud from './components/localCrud'
+import Searching from './components/Searching'
+import Filtering from './components/Filtering'
+import Sorting from './components/Sorting'
 
 export default function App() {
   return (
     <div>
-      <LocalCrud/>
+      {/* <LocalCrud/> */}
+      {/* <Searching/> */}
+      {/* <Filtering/> */}
+      <Sorting/>
     </div>
   )
 }
