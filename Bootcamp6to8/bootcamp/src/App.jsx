ee49b189b469231c0cda3ac8bc2e7a8566caf23c
Jitender Sarswat <114718722+jitender1322@users.noreply.github.com>
2025-03-05 19:03:18 +0530

    Update App.jsx

--- Bootcamp6to8/bootcamp/src/App.jsx
+++ Bootcamp6to8/bootcamp/src/App.jsx
@@ -1,15 +1,39 @@
+// import React from 'react'
+// import Calc from './Components/Calc'
+// import SimpleCrud from './Components/SimpleCrud'
+// import LocalCrud from './Components/LocalCrud'
+
+// export default function App() {
+//   return (
+//     <div>
+//       {/* <FunctionComponent/> */}
+//       {/* <Calc/> */}
+//       {/* <SimpleCrud/> */}
+//       <LocalCrud/>
+//     </div>
+//   )
+// } 
+
+
 import React from 'react'
-import Calc from './Components/Calc'
-import SimpleCrud from './Components/SimpleCrud'
-import LocalCrud from './Components/LocalCrud'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Filter from './filtering,Searching,Sorting/Filter'
+import Sorting from './filtering,Searching,Sorting/Sorting'
+import Searching from './filtering,Searching,Sorting/Searching'
+import Navbar from './Components/Navbar'
 
 export default function App() {
   return (
     <div>
-      {/* <FunctionComponent/> */}
-      {/* <Calc/> */}
-      {/* <SimpleCrud/> */}
-      <LocalCrud/>
+
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Filter />} ></Route>
+          <Route path='/sorting' Component={Sorting} ></Route>
+          <Route path='/searching/:id' Component={Searching} ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
-} 
+}
